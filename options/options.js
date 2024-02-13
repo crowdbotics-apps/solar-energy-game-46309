9ce1462ffbd0252391bce2913aea419765d0e5fd
Crowdbotics <team@crowdbotics.com>
2024-02-13 16:52:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solar-energy-game-46309",
+  url: "https://solar-energy-game-46309.botics.co",
+  api: "https://solar-energy-game-46309.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
